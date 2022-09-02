8c3e1bb40117f13c8f8b128fd2899c0b38b8e29b
Nadia Lopez <enadialopez@gmail.com>
2022-09-01 22:14:12 -0300

    agrego componentes

---
+++ src/components/Participant.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Participant = () => {
+    return (   
+        <> </>
+        )
+
+}
+
+export default Participant;
---
+++ src/components/Step.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Step = () => {
+    return (   
+        <> </>
+        )
+
+}
+
+export default Step;
