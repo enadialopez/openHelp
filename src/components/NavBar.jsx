3e10724d0ce1baa8c40f5a8ba5e69721ffd2d103
matiassalicru <mattiassalicru@gmail.com>
2022-09-03 11:43:22 -0300

    feat: fixed some styles and some containers, update class to classnames

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,11 @@ import '../styles/NavBar.css'
 
 const NavBar = () => {
     return (   
-        <>
           <div className="conteiner navbar">
-            <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-              <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white"> <span class="fs-4">OpenHelp!</span></a>
+            <header className="d-flex flex-wrap justify-content-center py-3 mb-4">
+              <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white" href='www.google.comgst'> <span class="fs-4">OpenHelp!</span></a>
             </header>
           </div>
-        </>
     )
 
 }
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
+import '../styles/Step.css';
 
 const Step = () => {
     return (   
-        <> 
-        <div class="conteiner">
-                <div class="row flex-lg-row align-items-center g-5 py-5">
-                    <div class="col-lg-6">
-                    <h1 class="fx-bold lh-4 mb-3">How to make your donation?</h1>
-                    <p class="lead">This is a very simple step by step guide for you to contribute to the planet 🌱</p>
-                    </div>
-                </div>
+        <div className="container row flex-lg-row align-items-center g-5 py-5">
+            <div className="col-lg-6">
+            <h1 className="fx-bold lh-4 mb-3">How to make your donation?</h1>
+            <p className="lead">This is a very simple step by step guide for you to contribute to the planet 🌱</p>
             </div>
-        </>
+        </div>
         )
 
 }
