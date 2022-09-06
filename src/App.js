b4f9381b9013e79485407b170914f64e7a234d44
Nadia Lopez <enadialopez@gmail.com>
2022-09-06 02:41:28 -0300

    agrego cards para los proyectos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Home from './components/Home'
 import About from './components/About'
 import Member from './components/Member'
 import NavBar from './components/NavBar'
-import Step from './components/Step';
+import Cards from './components/Cards'
+import Step from './components/Step'
 import './App.css'
+
 function App() {
     
-
   return (
     <>
       <NavBar />
       <div className='main_sections'>
         <Home />
         <About />
+        <Cards />
         <Step />
         <Member />
       </div>
---
+++ src/components/Project.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Project = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+export default Project;
