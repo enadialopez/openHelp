31568c03c973b502c7d3e56eae8e6b6e1c10ed36
matiassalicru <mattiassalicru@gmail.com>
2022-09-05 21:26:13 -0300

    feat: add Donations page, create a Button component

--- src/sections/Donations.js
+++ src/sections/Donations.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Button } from '../components/Button'
+import '../styles/Donations.css'
 
 export const Donations = () => {
   return (
-    <div className='d-flex flex-column'>
-      <div>
-        Card con botón de donación
-      </div>
+    <div className='donations-container'>
+     <div className='card'>
+        <h1 className='card-title'>Donate</h1>
+        <h3 className='card-subtitle'>You can any amount you want!</h3>
+        <div className='card-input-area'>
+          <input className='card-input' type='number' />
+          <p className='card-input-currency'>ETH</p>
+          <p className='card-input-caption'>Select the amount you want to donate to Openhelp!</p>
+        </div>
+        <Button text='Donate'/>
+        <p className='card-input-caption-bottom'>Thank you very much for your donation, it helps us a lot.</p>
+     </div>
     </div>
   )
 }
