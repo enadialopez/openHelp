b9ddc823378696cd92b9c61fcb758bd06ea54856
matiassalicru <mattiassalicru@gmail.com>
2022-09-08 18:25:17 -0300

    feat: add projects sections

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import "../styles/Card.css"
 
 function Card({ imageSource, title, text}) {
   return (    
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
-      <div className="overflow">
+    <div className="card text-center">
         <img src={imageSource} alt="a wallpaper" className="card-img-top" />
-      </div>
       <div className="card-body text-light">
         <h4 className="card-title">{title}</h4>
         <p className="card-text text-secondary">
